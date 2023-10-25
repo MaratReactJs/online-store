3b93fc10c636c736e14e9fb7e8a24f04fbb72c13
Assimbayev Marat <chesterfild-9@mail.ru>
2023-10-25 23:45:40 +0600

    header ready

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,28 @@
 import { LuMenuSquare } from 'react-icons/lu';
+import { FaUserAlt, FaShoppingCart } from 'react-icons/fa';
+import { ImLocation } from 'react-icons/im';
+
 export default function Header() {
 	return (
-		<header className="min-h-[10vh] bg-[#0193a9] flex items-center">
+		<header className="min-h-[10vh] bg-[#0193a9] flex items-center justify-around px-[2em]">
 			<LuMenuSquare className="text-[3em]" />
 			<h1 className="text-[2em]">ONLINE STORE</h1>
 			<input
 				type="search"
-				className="appearance-none border-none rounded  outline-none"
+				className="appearance-none border-none rounded-full  outline-none h-[3em] w-[60%]"
 			/>
+			<figure className="flex flex-col items-center">
+				<ImLocation className="text-[1.4em]" />
+				<figcaption>Адреса</figcaption>
+			</figure>
+			<figure className="flex flex-col items-center">
+				<FaUserAlt className="text-[1.4em]" />
+				<figcaption>Войти</figcaption>
+			</figure>
+			<figure className="flex flex-col items-center">
+				<FaShoppingCart className="text-[1.4em]" />
+				<figcaption>Корзина</figcaption>
+			</figure>
 		</header>
 	);
 }
